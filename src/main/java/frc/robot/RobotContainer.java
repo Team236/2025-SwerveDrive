f8fa09b148ff8fb81b18b0e663a4c4c63cf9ba62
Ellen DiCarlo <edicarlo@sbcglobal.net>
2025-01-09 13:58:33 -0500

    new controller, new controller buttons, new TargetSwerve command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.autos.*;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
@@ -20,17 +20,21 @@ import frc.robot.subsystems.*;
  */
 public class RobotContainer {
     /* Controllers */
-    private final Joystick driver = new Joystick(0);
+    //private final Joystick driver = new Joystick(0);
+    XboxController driver = new XboxController(Constants.Controller.USB_DRIVECONTROLLER);
+    XboxController auxController = new XboxController(Constants.Controller.USB_AUXCONTROLLER);
 
-//CONTROLLERS
- // XboxController driverController = new XboxController(Constants.Controller.USB_DRIVECONTROLLER);
- // XboxController auxController = new XboxController(Constants.Controller.USB_AUXCONTROLLER);
   //AUTO SWITCHES
   private static DigitalInput autoSwitch1 = new DigitalInput(Constants.Swerve.DIO_AUTO_1);
   private static DigitalInput autoSwitch2 = new DigitalInput(Constants.Swerve.DIO_AUTO_2);
   private static DigitalInput autoSwitch3 = new DigitalInput(Constants.Swerve.DIO_AUTO_3);
   private static DigitalInput autoSwitch4 = new DigitalInput(Constants.Swerve.DIO_AUTO_4);
 
+   //create instance of each subsystem  
+   private final Swerve s_Swerve = new Swerve();
+
+//DRIVE COMMANDS
+    private final TargetSwerve target1Swerve = new TargetSwerve(s_Swerve);
     
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -40,10 +44,6 @@ public class RobotContainer {
     /* Driver Buttons */
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-
-    /* Subsystems */
-    private final Swerve s_Swerve = new Swerve();
-
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -70,6 +70,39 @@ public class RobotContainer {
     private void configureButtonBindings() {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
+        
+ // XBOXCONTROLLER - DRIVER CONTROLLER
+    JoystickButton x = new JoystickButton(driver, Constants.XboxController.X);
+    JoystickButton a = new JoystickButton(driver, Constants.XboxController.A);
+    JoystickButton b = new JoystickButton(driver, Constants.XboxController.B);
+    JoystickButton y = new JoystickButton(driver, Constants.XboxController.Y);
+    JoystickButton lb = new JoystickButton(driver, Constants.XboxController.LB);
+    JoystickButton rb = new JoystickButton(driver, Constants.XboxController.RB);
+    JoystickButton lm = new JoystickButton(driver, Constants.XboxController.LM);
+    JoystickButton rm = new JoystickButton(driver, Constants.XboxController.RM);
+    JoystickButton view = new JoystickButton(driver, Constants.XboxController.VIEW);
+    JoystickButton menu = new JoystickButton(driver, Constants.XboxController.MENU);
+    POVButton upPov = new POVButton(driver,Constants.XboxController.POVXbox.UP_ANGLE);
+    POVButton downPov = new POVButton(driver,Constants.XboxController.POVXbox.DOWN_ANGLE); 
+    POVButton leftPov = new POVButton(driver,Constants.XboxController.POVXbox.LEFT_ANGLE);
+    POVButton rightPov = new POVButton(driver,Constants.XboxController.POVXbox.RIGHT_ANGLE);
+    // XBOX CONTROLLER - AUX CONTROLLER
+    JoystickButton x1 = new JoystickButton(auxController, Constants.XboxController.X);
+    JoystickButton a1 = new JoystickButton(auxController, Constants.XboxController.A);
+    JoystickButton b1 = new JoystickButton(auxController, Constants.XboxController.B);
+    JoystickButton y1 = new JoystickButton(auxController, Constants.XboxController.Y);
+    JoystickButton lb1 = new JoystickButton(auxController, Constants.XboxController.LB);
+    JoystickButton rb1 = new JoystickButton(auxController, Constants.XboxController.RB);
+    JoystickButton lm1 = new JoystickButton(auxController, Constants.XboxController.LM);
+    JoystickButton rm1 = new JoystickButton(auxController, Constants.XboxController.RM);
+    JoystickButton view1 = new JoystickButton(auxController, Constants.XboxController.VIEW);
+    JoystickButton menu1 = new JoystickButton(auxController, Constants.XboxController.MENU);
+    POVButton upPov1 = new POVButton(auxController,Constants.XboxController.POVXbox.UP_ANGLE);
+    POVButton downPov1 = new POVButton(auxController,Constants.XboxController.POVXbox.DOWN_ANGLE);
+    POVButton leftPov1 = new POVButton(auxController,Constants.XboxController.POVXbox.LEFT_ANGLE);
+    POVButton rightPov1 = new POVButton(auxController,Constants.XboxController.POVXbox.RIGHT_ANGLE);
+
+    a.whileTrue(target1Swerve);
     }
 
     /**
