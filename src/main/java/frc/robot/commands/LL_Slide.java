d12fdb11da8707f7c81b9810ef30f8a64916b7a7
sburnham3 <sburnham3@gmail.com>
2025-01-09 21:48:49 -0500

    changes similar to limelight doc kp targeting swerve example

--- src/main/java/frc/robot/commands/LL_Slide.java
+++ src/main/java/frc/robot/commands/LL_Slide.java
@@ -4,13 +4,16 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Swerve;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class LL_Slide extends Command {
+
 private Swerve swerve;
+private double Kp = 0.1;
 
   /** Creates a new LL_Slide. */
   public LL_Slide(Swerve s_Swerve) {
@@ -21,22 +24,27 @@ private Swerve swerve;
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    s_Swerve.setDefaultCommand(
-      new TeleopSwerve(
-          this.swerve, 
-          () -> 0, 
-          () -> -LimelightHelpers.getTY("limelight"), 
-          () -> 0,
-          true
-      )
-  );
-  
 
+  //TODO verify the limelighthelpers data should be horizontal relative to robot
+    // determine the target-to-robot matrix as [x, y, z, yaw, pitch, roll]
+      // x is the horizontal distance to the target - that we care about
+      // y is the forward distance to the target
+      // z is the vertical distance to the target    
+    double[] tar_robotMatrix = LimelightHelpers.getTargetPose_RobotSpace("limelight");
+
+    double x_feed = tar_robotMatrix[0] * Kp;
+    
+ //TODO verify the translation2d x directionis horizontal left right relative to robot
+    Translation2d translation2d= new Translation2d(-x_feed, 0.0);
+    swerve.drive(translation2d, 0.0, true, false);
+  }
+  
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
